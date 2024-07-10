a90444e1ce4900504af0da58e5969deb9791aab0
ryo_desktop <ryokawa1460@gmail.com>
2024-07-10 23:00:23 +0900

    0710

--- p5jstest/src/script.js
+++ p5jstest/src/script.js
@@ -1,19 +1,17 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
 
 /**
  * 宣言
  */
 //base
-let canvas, scene, camera, camera2,renderer, controls
+let canvas, scene, camera, renderer, controls
 
 //camera
 let fov
 
 //size
-const sizes = {width: window.innerWidth,height: window.innerHeight}
-//const sizes = {width: window.innerWidth,height: 500}
+const sizes = {width: window.innerWidth,height: window.innerHeight/2}
 
 //object
 let cursor1_mesh,sphere1
@@ -41,6 +39,12 @@ canvas = document.querySelector('canvas.webgl')
 // Scene
 scene = new THREE.Scene()
 
+//camera
+fov = 70
+camera = new THREE.PerspectiveCamera(fov, sizes.width / sizes.height, 0.01, dist(fov)*10)
+camera.position.set(0,0,dist(fov))
+scene.add(camera)
+
 /**
  * Renderer
  */
@@ -58,69 +62,8 @@ renderer.shadowMap.enabled = true
 renderer.setAnimationLoop(animate)
 /**renderer */
 
-/**
- * camera
- */
-//camera
-fov = 70
-camera = new THREE.PerspectiveCamera(fov, sizes.width / sizes.height, 0.01, dist(fov)*10)
-camera.position.set(0,0,dist(fov))
-scene.add(camera)
-
-camera2 =  new THREE.PerspectiveCamera(fov, sizes.width / sizes.height, 0.01, dist(fov)*10)
-camera2.position.set(0,0,dist(fov))
-scene.add(camera2)
-
-controls = new PointerLockControls( camera, canvas )
-document.addEventListener('click',() => {
-    if (!controls.isLocked){
-        controls.lock()
-    }else{
-        controls.unlock()
-    }
-})
-
-const rot_range = Math.PI/(18*3) //回転範囲
-const a = 10 //原点付近の傾き 値が高い方が傾斜が小さい (回転が遅い)
-const b = 5 //最高点にいくまでの距離　値が高い方が最高点が来るのが早い 5 ~ 15 くらいが良い
-
-const maxRotationY = rot_range
-const minRotationY = -rot_range
-
-const maxRotationX = rot_range
-const minRotationX = -rot_range
-
-const maxRotationZ = rot_range
-const minRotationZ = -rot_range
-
-let currentRotationY, currentRotationX, currentRotationZ
-
-window.addEventListener('mousemove',()=>{
-    //about camera
-    currentRotationX = camera.rotation.x
-    currentRotationY = camera.rotation.y
-    currentRotationZ = camera.rotation.z
-
-    //about camera2
-    //about X
-    if (currentRotationX >= 0 ) {
-        camera2.rotation.x = maxRotationX - (maxRotationX - currentRotationX / a)*Math.pow(Math.E,-currentRotationX * b)
-    } else if (currentRotationX < 0 ){
-        camera2.rotation.x = minRotationX - (minRotationX - currentRotationX / a)*Math.pow(Math.E,currentRotationX * b)
-    }
-    //about Y
-    if (currentRotationY >= 0 ){
-        camera2.rotation.y = maxRotationY - (maxRotationY - currentRotationY / a)*Math.pow(Math.E,-currentRotationY * b)
-    } else if (currentRotationY < 0 ) {
-        camera2.rotation.y = minRotationY - (minRotationY - currentRotationY / a)*Math.pow(Math.E,currentRotationY * b)
-    }
-    //about Z
-    if (currentRotationZ >= 0 ){
-        camera2.rotation.z = maxRotationZ - (maxRotationZ - currentRotationZ / a)*Math.pow(Math.E,-currentRotationZ * b)
-    } else if (currentRotationZ < 0 ) {
-        camera2.rotation.z = minRotationZ - (minRotationZ - currentRotationZ / a)*Math.pow(Math.E,currentRotationZ * b)
-    }
-})
+//controls
+controls = new OrbitControls( camera, canvas)
 
 /**
  * Object
@@ -133,16 +76,6 @@ sphere1 = new THREE.Mesh(
     })
 )
 scene.add(sphere1)
-
-//plate
-const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(2.0,2.0,10,10),
-    new THREE.MeshStandardMaterial({
-        color:0xff0000,wireframe:true
-    })
-)
-plane.position.set(0,0,-0.5)
-scene.add(plane)
 
 //cursor
 cursor1_mesh = new THREE.Mesh(
@@ -208,16 +141,11 @@ function onWindowResize(){
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
-    //sizes.height = 500
 
     // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.position.set(0,0,dist(fov))
     camera.updateProjectionMatrix()
-
-    camera2.aspect = sizes.width / sizes.height
-    camera2.position.set(0,0,dist(fov))
-    camera2.updateProjectionMatrix()
 
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
@@ -235,15 +163,12 @@ function WindowFullscreen(){
 
 //loop animation
 function animate(){
-    //controls.update()
+    controls.update()
     // Render
-    //camera.lookAt(0,0,0)
+    renderer.render(scene, camera)
 
     //second
     const sec = performance.now()/1000
-
-
-    renderer.render(scene, camera2)
 }
 /**Function */
 
@@ -260,9 +185,6 @@ window.addEventListener("dblclick",WindowFullscreen)
 document.addEventListener("keydown",(e)=>{
     if(e.keyCode == 49) {
         camera.position.set(0,0,dist(fov))
-        camera.rotation.set(0,0,0)
-        camera2.position.set(0,0,dist(fov))
-        camera2.rotation.set(0,0,0)
     }
     if(e.keyCode == 50) {
         camera.position.set(dist(fov),0,0)
@@ -279,11 +201,6 @@ document.addEventListener("keydown",(e)=>{
     if(e.keyCode == 54) {
         camera.position.set(0,-dist(fov),0)
     }
-    if(e.keyCode == 48) {
-        camera.position.set(0,0,2*dist(fov))
-    }
-    camera.updateProjectionMatrix()
-    camera2.updateProjectionMatrix()
 })
 
 //mouse
---
+++ p5jstest/src/sketch.js
@@ -0,0 +1,68 @@
+import * as p5 from 'p5'
+
+/**
+ * Initialization
+ */
+//const canvas = document.querySelector('canvas.p5')
+const sizes = {width: window.innerWidth,height: window.innerHeight}
+
+let bwval = 0
+let rgval = 100
+/**Initialization */
+
+/**
+ * p5
+ */
+new p5((p) => {
+    p.setup = () => {
+        p.createCanvas(sizes.width, sizes.height)
+    }
+
+    p.draw = () => {
+        //background
+        p.background(200)
+
+        //square
+        p.fill(bwval)
+        p.strokeWeight(1)
+        p.rect(10,10,400,200)
+
+        //square2
+        p.fill(rgval)
+        p.strokeWeight(2)
+        p.rect(10,300,400,200)
+
+        //mouse cursor
+        p.fill(255)
+        p.strokeWeight(4)
+        p.ellipse(p.mouseX, p.mouseY, 50, 50)
+    }
+
+    p.mouseClicked = () => {
+        if (rgval == 100){
+            rgval = 175
+        } else {
+            rgval = 100
+        }
+    }
+})
+
+window.addEventListener('click',()=>{
+    if (bwval == 0){
+        bwval = 255
+    } else {
+        bwval = 0
+    }
+    console.log('triggered' + bwval)
+})
+
+/**
+function setup() {
+    createCanvas(400, 400)
+}
+
+function draw() {
+    background(220)
+    ellipse(mouseX, mouseY, 50, 50)
+}
+*/
